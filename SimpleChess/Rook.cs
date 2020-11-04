bcf83cd0de0e0d7d08c82dbcb1b5d1ade9fdc2b2
henninghk <64491564+henninghk@users.noreply.github.com>
2020-11-04 13:38:41 +0100

    411

---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        public Rook() : base("TRN")
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+
+        }
+    }
+}
